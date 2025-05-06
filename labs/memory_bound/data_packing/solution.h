aba3188c43cfdfd38c2098711fced12abfc4683b
Kyotak <yuhongh@qq.com>
2025-05-06 20:27:36 +0800

    update data_packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,15 +6,36 @@ constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
-struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+#ifdef SOLUTION
+
+struct S
+{
+  float d;
+  long long l : 16;
+  int i : 8;
+  short s : 8;
+  bool b : 1;
+  bool operator<(const S &s) const { return this->i < s.i; }
+};
+
+#else
+struct S
+{
+  int i;       // 4
+  long long l; // 8
+  short s;     // 2
+  double d;    // 8
+  bool b;      // 1
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+#endif
+
+// template <int N>
+// class TD;
+
+// TD<sizeof(S)> td;
 
 void init(std::vector<S> &arr);
 S create_entry(int first_value, int second_value);
